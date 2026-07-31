412ccbd59a95325c33654670a8a73ae4501f278d
unknown <jiyachaudhary484@gamil.com>
2026-07-31 11:39:46 +0530

    Added premium loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Loader from "./components/Loader";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -8,6 +12,20 @@ import Contact from "./pages/Contact";
 import WhatsAppButton from "./components/WhatsAppButton";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <BrowserRouter>
       <Routes>
@@ -17,6 +35,7 @@ function App() {
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
       <WhatsAppButton />
     </BrowserRouter>
   );
---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+function Loader() {
+    return (
+        <div className="loader">
+            <h1>OM Pictures</h1>
+            <div className="loader-line"></div>
+        </div>
+    );
+}
+
+export default Loader;
